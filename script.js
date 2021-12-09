3689aaa0d67d30d7070c09dbfad317db30715cbc
Ebube Jude <ejude44@yahoo.com>
2021-12-10 00:18:57 +0100

    Add files via upload
    omor

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const name = document.querySelector(".name");
 
 const popup = document.querySelector(".popup");
 
+const popups = document.getElementById("myPopup");
+
 
 
 // Oh yes button
@@ -30,48 +32,242 @@ document.querySelector(".ohYes").addEventListener("click", function(){
 
 // btn-primary
 
-const click =document.querySelector(".btn-primary").addEventListener("click", function(){
+const click =document.querySelector(".btn-primary").addEventListener("click", function(e){
 
-   const birth = document.querySelector(".birth").value;
+  e.preventDefault();
+  const birth = document.querySelector(".birth").value;
+  const age = document.querySelector('.age').value;
+  const acct = document.querySelector('.acct').value;
+  const destination = document.querySelector('.destination').value;
 
-   let score=0;
+  const checkedPolitician= document.querySelector('input[type = radio ]:checked').value;
+  const educationLevel = document.getElementById("education").value;
 
-   if(birth === "abuja" || birth ==="lagos"){
-        score = score + 10;
+  const marital = document.getElementById("marital").value;
 
-   }else {
-       score = 5;
-   }
+  const checkedPassport = document.querySelector('input[name = passport]:checked').value;
 
-   const names = [];
+  const employment = document.getElementById("employment").value;
 
-
-   //pop up function
    
-    const popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
 
- 
+
+  //employment
+let employmentScore = 0;
+
+const employFunction = function(){
+  if(employment === "employed"){
+    employmentScore = employmentScore + 10
+  } else if( employment==="unemployed"){
+    employmentScore = employmentScore + 5
+  }
+  return employmentScore;
+}
+
+console.log( employFunction(employment));
+
+
+  //passport
+  let passportScore = 0;
+
+  const passportFunction = function(){
+    if(checkedPassport=== "nopassport"){
+      passportScore = passportScore + 5;
+    } else if(checkedPassport === "passport"){
+      passportScore = passportScore + 15;
+    }
+    else{
+      passportScore = passportScore + 0;
+    }
+    return passportScore;
+  }
+   console.log( passportFunction(checkedPassport));
+
+// // marital
+let maritalScore =0;
+ const maritalFunction = function(){
+   if(marital==="single"){
+     maritalScore = maritalScore + 5;
+   } else if(marital === "married"){
+     maritalScore = maritalScore +10
+   }
+   return maritalScore;
+ }
+ console.log( maritalFunction(marital));
+
+
+// // education 
+
+let eduScore = 0;
+function chainToSwitchs(vals) {
+  var answer = educationLevel;
+  // Only change code below this line
+
+  switch (vals) {
+    case "schoolcert":
+      answer = 2;
+      break;
+    case "bachelors":
+      answer = 6;
+      break;
+    case "masters":
+      answer = 8;
+      break;
+    case "phd":
+      answer = 10;
+      break;
+    case "drop-out":
+      answer = 0;
+      break;
+      
+  }
+  
+
+  // Only change code above this line
+ eduScore = eduScore + answer;
+   return answer;
+
+}
+console.log( chainToSwitchs(educationLevel));
+
 
 
   
-  const popscore = document.querySelector(".popscore");
-  popscore.textContent = score;
 
+
+//   //Politican
+let politicianScore = 0;
+const politicianFunc = function(){
+  if (checkedPolitician === "no") {politicianScore = politicianScore + 0; }else {politicianScore = politicianScore + 10;}
+
+  return politicianScore;
+}
+console.log( politicianFunc(checkedPolitician));
+
+
+  // //age
+  let agescore = 0;
+  const ageFunc = function(){
+    if(age <= 23 || age > 50) {
+      
+       agescore = agescore + 10;
+    } else if (age > 23  && age < 50){
+      agescore = agescore + 5;
+    }
+    else{
+      agescore = agescore +0;
+    }
+    return agescore;
+  }
+  console.log( ageFunc(age));
+
+  // // acctbalance
+  let acctscore = 0;
+  const balance = function(){
+    if(acct >= 1000000) {
+      
+       acctscore = acctscore + 15;
+    } else if (acct >= 500000 &&  acct < 1000000 ){
+      acctscore = acctscore + 10;
+    } else {
+      acctscore = acctscore +5
+    }
+    return acctscore;
+    
+  }
+  console.log( balance(acct));
+  // //Birthplace
+  let birthscore = 0;
+   const placeOfBirth = function(str){
+     if(str=== 'lagos' || str === 'abuja'){
+       
+        birthscore = birthscore + 10;
+     } else {
+       birthscore = birthscore + 5;
+     }
+     return birthscore
+   }
+   console.log( placeOfBirth(birth));
+
+//    //travel
+   let travelScore = 0;
+function chainToSwitch(val) {
+  var answer = destination;
+  // Only change code below this line
+
+  switch (val) {
+    case "usa":
+      answer = 5;
+      break;
+    case "uk":
+      answer = 5;
+      break;
+    case "usa":
+      answer = 5;
+      break;
+    case "canada":
+      answer = 5;
+      break;
+    case "germany":
+      answer = 5;
+      break;
+      case destination:
+        answer = 10;
+        break;
+  }
+
+  // Only change code above this line
+ travelScore = travelScore+ answer;
+   return answer;
+
+}
+
+// Change this value to test
+console.log( chainToSwitch(destination));
+
+  //total score
+
+   const totalScore = birthscore + agescore + acctscore + travelScore + politicianScore + eduScore + maritalScore + passportScore + employmentScore;
+
+   if (totalScore >= 85){
+    document.getElementById("image").src = "/docs/dispensation.png";
+   } else if(totalScore >65 ){
+    document.getElementById("image").src = "/docs/comrade.jpg";
+
+   } else if(totalScore< 65){
+    document.getElementById("image").src = "/docs/sabinus.jpg";
+
+   }
+
+  const popscore = document.querySelector(".popscore");
+  popscore.textContent = totalScore;
+
+  //pop up 
+   
+
+  popups.classList.toggle("show");
 
 })
 
+
+  
+
+
+
+   
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' || e.key=== 'Enter' && !popup.classList.contains('hidden')) {
     popup.classList.add("hidden");
   }
 });
 
+const close = document.querySelector(".close").addEventListener("click", function(){
+
+  popup.classList.add("hidden");
 
 
-
-
-
+}
+)
 
 
 
